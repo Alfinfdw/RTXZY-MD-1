3c972ef22662b98b34f55511c37ebe7dcbd38834
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2023-04-13 11:11:05 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -652,7 +652,7 @@ module.exports = {
         } catch (e) {
             console.error(e)
         } finally {
-             conn.sendPresenceUpdate('recording', m.chat)
+             conn.sendPresenceUpdate('composing', m.chat)
             //console.log(global.db.data.users[m.sender])
             let user, stats = global.db.data.stats
             if (m) {
