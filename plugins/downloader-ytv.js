2a2f351c9305f4bdef5d763c3bf129a8ca55b070
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2023-04-14 20:42:29 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,62 +1,58 @@
-var limit = 50
-var {
-	youtubeSearch,
-	youtubedl,
-	youtubedlv2,
-	youtubedlv3
-} = require('@bochilteam/scraper')
-var fs = require('fs')
+let limit = 80
+import fetch from 'node-fetch'
+import { youtubeSearch, youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, video: _video, title} = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
+  let video, source, res, link, lastError, isLimit
+  for (let i in _video) {
+    try {
+      video = _video[i]
+      isLimit = limitedSize < video.fileSize
+      if (isLimit) continue
+      link = await video.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      video = source = link = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
+  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+*${htki} YOUTUBE ${htka}*
 
-var handler = async (m, {
-	conn,
-	args,
-	isPrems,
-	isOwner,
-	command,
-	text,
-	usedPrefix
-}) => {
-	if (!text) throw 'Link nya mana?'
-	var chat = db.data.chats[m.chat]
-	var vid = (await youtubeSearch(text)).video[0]
-	if (!vid) throw 'Video/Audio Tidak ditemukan'
-	var {
-		videoId
-	} = vid
-	var url = 'https://www.youtube.com/watch?v=' + videoId
-	var {
-		thumbnail,
-		video: _video,
-		title
-	} = await youtubedl(url).catch(async _ => await youtubedlv2(url)).catch(async _ => await youtubedlv3(url))
-	var limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
-	var video, source, res, link, lastError, isLimit
-	for (var i in _video) {
-		video = _video[i]
-		isLimit = limitedSize < video.fileSizeH
-		if (isLimit) continue
-		link = await video.download()
-		if (link) res = await fetch(link)
-		isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
-		if (isLimit) continue
-		if (res) source = await res.arrayBuffer()
-		if (source instanceof ArrayBuffer) break
-	}
-	var _thumb = {}
-	try {
-		_thumb = {
-			thumbnail: await (await fetch(thumbnail)).buffer()
-		}
-	} catch (e) {}
-	conn.sendFile(m.chat, link, title + '.mp4', `
-*🔥 Title:* ${title}
-*📁 File Size:* ${video.fileSizeH}
+*${htjava} Title:* ${title}
+*${htjava} Quality:* 360p
+*${htjava} Filesize:* ${video.fileSizeH}
+`.trim(), m)
+  let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
+  catch (e) { }
+  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
+*${htki} YOUTUBE ${htka}*
+
+*${htjava} Title:* ${title}
+*${htjava} Quality:* 360p
+*${htjava} Filesize:* ${video.fileSizeH}
 `.trim(), m, false, {
-		..._thumb,
-		asDocument: chat.useDocument
-	})
+    ..._thumb,
+    asDocument: chat.useDocument
+  })
 }
-handler.help = ['ytmp4']
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
-handler.command = ['ytmp4', 'ytv']
-module.exports = handler
+handler.command = /^yt(v|mp4)?$/i
+
+handler.exp = 0
+handler.register = false
+handler.limit = true
+
+
+export default handler
